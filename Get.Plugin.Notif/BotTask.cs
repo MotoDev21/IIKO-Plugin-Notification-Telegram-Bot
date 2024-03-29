9c6613f13864c74a0572a74c1bf7c7bb8dc117e7
matvey.nizamutdinov <matvey.nizamutdinov@DESKTOP-ODO1JRT>
2024-03-29 10:32:05 +0300

    Добавьте файлы проекта.

---
+++ Get.Plugin.Notif/BotTask.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using System.Runtime.InteropServices;
+using static Get.Plugin.Notif.MyPlugin;
+
+namespace Get.Plugin.Notif
+{
+    internal class BotTask
+    {
+        public static string NotificationBot = "Уведомление от бота";
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+        public static void BotWorker()
+        {
+            var botClient = new TelegramBotClient("6347892364:AAH2BMZ08X9********************"); //Ваш токен бота
+            using (var cts = new CancellationTokenSource())
+            {
+                // Настройка получения обновлений
+                var receiverOptions = new ReceiverOptions
+                {
+                    AllowedUpdates = Array.Empty<UpdateType>() // Получать все типы обновлений
+                };
+
+                botClient.StartReceiving(
+                    updateHandler: HandleUpdateAsync,
+                    pollingErrorHandler: HandleErrorAsync,
+                    receiverOptions: receiverOptions,
+                    cancellationToken: cts.Token);
+
+                MessageBox(new IntPtr(0), "Бот запущен!", "TelegramNotification", 0);
+            }
+        }
+
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Проверяем, есть ли сообщение
+            if (update.Type != UpdateType.Message || update.Message.Type != MessageType.Text)
+                return;
+
+            var chatId = update.Message.Chat.Id;
+            var messageText = update.Message.Text;
+
+            //MessageBox(new IntPtr(0), $"Получено новое сообщение: {messageText}", "TelegramNotification", 0);
+            PluginStart(messageText);
+            await botClient.SendTextMessageAsync(chatId, $"Вы сказали: {messageText}", cancellationToken: cancellationToken);
+
+        }
+
+        public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            string ErrorMessage = "Произошла ошибка";
+            if (exception is ApiRequestException apiRequestException)
+            {
+                ErrorMessage = $"Ошибка Telegram API:\n{apiRequestException.ErrorCode}\n{apiRequestException.Message}";
+            }
+            else
+            {
+                ErrorMessage = exception.ToString();
+            }
+
+            MessageBox(new IntPtr(0), ErrorMessage, "TelegramNotification", 0);
+            return Task.CompletedTask;
+        }
+    }
+
+    
+}
+
---
+++ Get.Plugin.Notif/Class1.cs
@@ -0,0 +1,86 @@
+﻿using Resto.Front.Api;
+using Resto.Front.Api.Attributes;
+using Resto.Front.Api.Data.Organization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Get.Plugin.Notif.BotTask;
+
+namespace Get.Plugin.Notif
+{
+    [PluginLicenseModuleId(******)] //Ваш ID Лицензии 
+   
+        public sealed class MyPlugin : IFrontPlugin
+        {
+            public static readonly string Sender = "WRF_Matvey";
+
+            [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+            public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+            public MyPlugin()
+            {
+            MessageBox(new IntPtr(0), "Плагин запущен!", "Уведомление плагина", 0);
+            PluginStart("Плагин запущен!");
+            BotWorker();
+            }
+
+            public static void PluginStart(string NotificationBotText)
+            {
+            try
+            {
+                ITerminalsGroup hostTerminalsGroup = PluginContext.Operations.GetHostTerminalsGroup();
+                ITerminal hostTerminal = PluginContext.Operations.GetHostTerminal();
+                //var logger = PluginContext.Log;
+                // Вызовs функции ShowOkPopup
+                PluginContext.Operations.AddNotificationMessage(NotificationBotText, MyPlugin.Sender, new TimeSpan?(TimeSpan.FromSeconds(40.0)));
+                PluginContext.Operations.AddNotificationMessage(hostTerminalsGroup.Name, MyPlugin.Sender, new TimeSpan?(TimeSpan.FromSeconds(40.0)));
+                PluginContext.Operations.AddNotificationMessage(hostTerminal.Name, MyPlugin.Sender, new TimeSpan?(TimeSpan.FromSeconds(40.0)));
+                PluginContext.Log.Info(MyPlugin.Sender + " started!");
+            }
+            catch (Exception ex)
+            {
+                PluginContext.Log.Error(string.Format("{0}", (object)ex));
+            }
+            }
+
+
+            // add your plugin logic here
+            private bool disposedValue;
+
+            private void Dispose(bool disposing)
+            {
+                if (!disposedValue)
+                {
+                    if (disposing)
+                    {
+                        // TODO: освободить управляемое состояние (управляемые объекты)
+                    }
+
+                    // TODO: освободить неуправляемые ресурсы (неуправляемые объекты) и переопределить метод завершения
+                    // TODO: установить значение NULL для больших полей
+                    disposedValue = true;
+                }
+            }
+
+            // // TODO: переопределить метод завершения, только если "Dispose(bool disposing)" содержит код для освобождения неуправляемых ресурсов
+            // ~MyPlugin()
+            // {
+            //     // Не изменяйте этот код. Разместите код очистки в методе "Dispose(bool disposing)".
+            //     Dispose(disposing: false);
+            // }
+
+            void IDisposable.Dispose()
+            {
+                // Не изменяйте этот код. Разместите код очистки в методе "Dispose(bool disposing)".
+                Dispose(disposing: true);
+                GC.SuppressFinalize(this);
+            }
+        }
+
+
+
+    }
+
